39872b8488191695a3efb2e13c6d927f1eff00ce
winsoft666 <winsoft666@outlook.com>
2019-05-22 14:22:19 +0800

    update CMakeLists.txt

--- build.bat
+++ build.bat
@@ -5,3 +5,5 @@ vcpkg install gtest:x86-windows
 cmake.exe -G "Visual Studio 14" -DCMAKE_TOOLCHAIN_FILE=D:\sourcecode\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x86-windows -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=D:\ppxnet -DPPXBASE_INSTALL_DIR=D:\ppxbase -DBUILD_TESTS=ON -S %~dp0 -B %~dp0build
 
 cmake.exe --build %~dp0build
+
+pause
--- run_cmake.bat
+++ run_cmake.bat
@@ -1,2 +1,3 @@
 @echo off
-cmake.exe -G "Visual Studio 14" -DCMAKE_TOOLCHAIN_FILE=D:\sourcecode\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x86-windows -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=D:\ppxnet -DPPXBASE_INSTALL_DIR=D:\ppxbase -DBUILD_TESTS=ON -S %~dp0 -B %~dp0build
+cmake.exe -G "Visual Studio 14" -DCMAKE_TOOLCHAIN_FILE=D:\sourcecode\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x86-windows-static -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=D:\ppxnet -DBUILD_TESTS=ON -S %~dp0 -B %~dp0build
+pause
