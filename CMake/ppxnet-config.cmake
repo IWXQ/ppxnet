b4e4091065a93925166894d6c1e51c5aa2477c49
winsoft666 <winsoft666@outlook.com>
2019-05-23 16:53:13 +0800

    check if cmake file exist.

--- CMake/ppxnet-config.cmake
+++ CMake/ppxnet-config.cmake
@@ -2,5 +2,10 @@
 
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
-include(${SELF_DIR}/ppxnet-target.cmake)
-include(${SELF_DIR}/ppxnet-static-target.cmake)
+if(EXISTS ${SELF_DIR}/ppxnet-target.cmake)
+	include(${SELF_DIR}/ppxnet-target.cmake)
+endif()
+
+if(EXISTS ${SELF_DIR}/ppxnet-static-target.cmake)
+	include(${SELF_DIR}/ppxnet-static-target.cmake)
+endif()
