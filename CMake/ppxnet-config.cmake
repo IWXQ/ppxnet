e90551d043aaf8d1eb2bc7625a505d33d073a47a
winsoft666 <winsoft666@outlook.com>
2019-05-23 11:28:22 +0800

    build script.

--- CMake/ppxnet-config.cmake
+++ CMake/ppxnet-config.cmake
@@ -2,13 +2,9 @@
 
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
-if(CMAKE_BUILD_TYPE STREQUAL "" OR NOT DEFINED CMAKE_BUILD_TYPE)
-	include(${SELF_DIR}/ppxnet-target.cmake)
-else()
-	include(${SELF_DIR}/ppxnet-target-${CMAKE_BUILD_TYPE}.cmake)
-endif()
+include(${SELF_DIR}/ppxnet-target.cmake)
 
 if(TARGET ppxnet)
 	get_target_property(PPXNET_INCLUDE_DIRS ppxnet INTERFACE_INCLUDE_DIRECTORIES)
-	get_target_property(PPXNET_LIBRARIES ppxnet "LOCATION${CMAKE_BUILD_TYPE}")
+	get_target_property(PPXNET_LIBRARIES ppxnet "LOCATION_${CMAKE_BUILD_TYPE}")
 endif()
